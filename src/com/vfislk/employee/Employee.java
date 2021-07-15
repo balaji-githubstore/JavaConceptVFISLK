92b372db7ca667830e080e3b03ef5ba9fffb766f
balaji-githubstore <balaji0017@gmail.com>
2021-07-15 13:01:28 +0530

    print employee detail - static method

---
+++ src/com/vfislk/employee/Employee.java
@@ -0,0 +1,25 @@
+package com.vfislk.employee;
+
+public class Employee {
+	
+	public int empId;
+	public String empName;
+	public double empSalary;
+	public static String companyName;
+	
+	
+	public static void printEmployeeDetail(Employee emp)
+	{
+		System.out.println("------------------");
+		System.out.println(emp);
+		System.out.println("Employee id "+emp.empId);
+		System.out.println("Employee name "+emp.empName);
+		System.out.println(emp.empSalary);
+		System.out.println(Employee.companyName);
+		System.out.println("------------------");
+	}
+	
+	
+	
+
+}
---
+++ src/com/vfislk/employee/EmployeeTest.java
@@ -0,0 +1,44 @@
+package com.vfislk.employee;
+
+public class EmployeeTest {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println(Employee.companyName);
+		Employee.companyName="VFI-SLK";
+		
+		Employee emp1=new Employee();
+		
+		Employee emp2=new Employee();
+		
+		
+		emp1.empId=101;
+		emp1.empName="John";
+		emp1.empSalary=5000;
+
+		
+		emp2.empId=102;
+		emp2.empName="Peter";
+		emp2.empSalary=4000;
+		
+		System.out.println(emp1);
+		Employee.printEmployeeDetail(emp1);
+		
+		System.out.println(emp2);
+		Employee.printEmployeeDetail(emp2);
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+
+
+
+
